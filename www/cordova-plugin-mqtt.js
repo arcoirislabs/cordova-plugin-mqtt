6d33f711f12e0bc478fa81bc9d711426493e7703
Rahul Kundu <rahul.kundu@here.com>
2017-07-28 20:03:16 +0530

    Fixing KeepAlive issue

--- www/cordova-plugin-mqtt.js
+++ www/cordova-plugin-mqtt.js
@@ -103,7 +103,7 @@ channel = require('cordova/channel'),
                     }
                 }, function(e){
                     console.error(e);
-                }, "CordovaMqTTPlugin", "connect", [url,(args.clientId,args.keepAlive === undefined ? 60000 : args.clientId,args.keepAlive),iscls,args.connectionTimeout||30,args.username, args.password,args.willTopicConfig.topic,args.willTopicConfig.payload,args.willTopicConfig.qos||0,(args.willTopicConfig.retain === undefined ? true : args.willTopicConfig.retain),args.version||"3.1.1"]);
+                }, "CordovaMqTTPlugin", "connect", [url,args.clientId,(args.keepAlive === undefined ? 60000 : args.keepAlive),iscls,args.connectionTimeout||30,args.username, args.password,args.willTopicConfig.topic,args.willTopicConfig.payload,args.willTopicConfig.qos||0,(args.willTopicConfig.retain === undefined ? true : args.willTopicConfig.retain),args.version||"3.1.1"]);
             } else {
 
                 if (args.url.split("tcp://").length > 1) {
